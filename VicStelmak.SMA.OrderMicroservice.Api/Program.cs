483ecde3bddad2a7b3082b9110d6594287c2c407
VicStelmak <vstelmak@protonmail.com>
2024-03-08 21:24:28 +0300

    Reconfigured Previosly Created Structure of OrderMicroservice
    To reconfigure the previosly created structure of OrderMicroservices deleted all of the projects inside of this microservice and added VicStelmak.SMA.OrderMicroservice.API and VicStelmak.SMA.OrderMicroservice.DataLibrary to the above-mentioned microservice. Also referenced VicStelmak.SMA.OrderMicroservice.DataLibrary in VicStelmak.SMA.OrderMicroservice.API, reconfigured the Program.cs of VicStelmak.SMA.OrderMicroservice.API accordingly, referenced VicStelmak.SMA.Events and VicStelmak.SMA.MessageBrokerSettings in VicStelmak.SMA.OrderMicroservice.API and added VicStelmak.SMA.OrderMicroservice.API and VicStelmak.SMA.UserMicroservice.API as the startup projects to the solution.

--- VicStelmak.SMA.OrderMicroservice.Api/Program.cs
+++ VicStelmak.SMA.OrderMicroservice.Api/Program.cs
@@ -1,12 +1,9 @@
-using VicStelmak.SMA.OrderMicroservice.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.ConfigureDependencyInjection(builder.Configuration);
 
 var app = builder.Build();
 
