9d0c740849c820536d53521b4fe23d9ea3da3971
VicStelmak <vstelmak@protonmail.com>
2024-03-29 12:29:19 +0300

    Reconfigured Previosly Created Structure of UserMicroservice
    To reconfigure the previosly created structure of UserMicroservice deleted project which initially represented this microservice and added VicStelmak.SMA.UserMicroservice.API and VicStelmak.SMA.UserMicroservice.APIDataLibrary to the above-mentioned microservice. Also referenced VicStelmak.SMA.UserMicroservice.APIDataLibrary in VicStelmak.SMA.UserMicroservice.API, reconfigured the Program.cs of VicStelmak.SMA.UserMicroservice.API (added the line regards to authentication), referenced VicStelmak.SMA.Events and VicStelmak.SMA.MessageBrokerSettings in VicStelmak.SMA.UserMicroservice.API and added VicStelmak.SMA.UserMicroservice.API as one of the startup projects to the solution.

--- VicStelmak.SMA.UserMicroservice.API/Program.cs
+++ VicStelmak.SMA.UserMicroservice.API/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,5 +17,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
